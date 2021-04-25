21fd5568801c1cdba5629f239e6e91e5aad6af8c
Devon Eilers <devon.d.eilers@gmail.com>
2021-04-25 13:08:49 -0700

    add arch-specific dockerfiles and setup scripts

---
+++ setup_btc_amd64.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+apt-get update
+apt-get install wget
+
+wget https://bitcoincore.org/bin/bitcoin-core-0.21.0/bitcoin-0.21.0-x86_64-linux-gnu.tar.gz
+wget https://bitcoincore.org/bin/bitcoin-core-0.21.0/SHA256SUMS.asc
+wget https://bitcoin.org/laanwj-releases.asc
+
+sha256sum SHA256SUMS.asc --ignore-missing > checksum
+sha256sum -c checksum
+
+gpg --import ./laanwj-releases.asc
+gpg --refresh-keys
+gpg --verify SHA256SUMS.asc
+
+tar -xvf bitcoin-0.21.0-x86_64-linux-gnu.tar.gz
+install -m 0755 -o root -g root -t /usr/local/bin bitcoin-0.21.0/bin/*
+
+rm bitcoin-0.21.0-x86_64-linux-gnu.tar.gz
+rm -rf bitcoin-0.21.0
--- setup_btc_pi.sh
+++ setup_btc_pi.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install wget
 
-wget https://bitcoincore.org/bin/bitcoin-core-0.21.0/bitcoin-0.21.0-x86_64-linux-gnu.tar.gz
+wget https://bitcoincore.org/bin/bitcoin-core-0.21.0/bitcoin-0.21.0-aarch64-linux-gnu.tar.gz
 wget https://bitcoincore.org/bin/bitcoin-core-0.21.0/SHA256SUMS.asc
 wget https://bitcoin.org/laanwj-releases.asc
 
@@ -13,5 +13,8 @@ gpg --import ./laanwj-releases.asc
 gpg --refresh-keys
 gpg --verify SHA256SUMS.asc
 
-tar -xvf bitcoin-0.21.0-x86_64-linux-gnu.tar.gz
+tar -xvf bitcoin-0.21.0-aarch64-linux-gnu.tar.gz
 install -m 0755 -o root -g root -t /usr/local/bin bitcoin-0.21.0/bin/*
+
+rm bitcoin-0.21.0-aarch64-linux-gnu.tar.gz
+rm -rf bitcoin-0.21.0
